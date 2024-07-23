c49243b23892cc3e41afe783997af1f9fb4eecc3
awr7 <147633136+awr7@users.noreply.github.com>
2024-07-23 12:51:30 -0400

    added more comments to the database

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -14,6 +14,24 @@ class CommentSeeder extends Seeder
             Comment::factory()->count(rand(50, 200))->create([
                 'user_id' => $user->id,
             ]);
+
+            // more comments for two days ago
+            Comment::factory()->count(rand(10, 50))->create([
+                'user_id' => $user->id,
+                'created_at' => now()->subDays(2),
+            ]);
+
+            // more comments for yesterday
+            Comment::factory()->count(rand(10, 50))->create([
+                'user_id' => $user->id,
+                'created_at' => now()->subDay(),
+            ]);
+
+            // more comments for today
+            Comment::factory()->count(rand(10, 50))->create([
+                'user_id' => $user->id,
+                'created_at' => now(),
+            ]);
         });
     }
 }
